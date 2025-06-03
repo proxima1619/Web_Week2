f5fa7fe4c56d3f46946dd0f7072d8b7561693981
proxima1619 <proxima1619@gmail.com>
2025-06-03 19:33:24 +0900

    백엔드 초기 설정
    backend 필요한 패키지 설치
    server.js 파일 생성
    .env 파일 생성

---
+++ mern-board/backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// 테스트용 라우터
+app.get('/', (req, res) => {
+  res.send('백엔드 서버 연결 완료!');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
